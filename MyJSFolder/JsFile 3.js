9c710b81dd1d847405e2d615072a28ec94f3bc59
Emeka mordi Chinomso <emekamordijoseph@gmail.com>
2018-10-16 22:17:49 -0700

    My first commit

---
+++ MyJSFolder/JsFile 3.js
@@ -0,0 +1,6 @@
+<button class="tablinks" onclick="openCity(event, 'London')" id="defaultOpen">London</button>
+
+<script>
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+</script>
